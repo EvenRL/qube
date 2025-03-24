70a88a04bd2b092dc3dd3894de3b7148d0e175fb
EvenRL <52103879+EvenRL@users.noreply.github.com>
2025-03-24 20:12:41 +0100

    Add gui argument and argument descriptions

--- qube_bringup/launch/bringup.launch.py
+++ qube_bringup/launch/bringup.launch.py
@@ -2,6 +2,7 @@ from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, Opaq
 from launch.substitutions import PathJoinSubstitution, TextSubstitution, LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
+from launch.conditions import IfCondition, UnlessCondition
 from launch_ros.substitutions import FindPackageShare
 from launch import LaunchDescription
 from launch_ros.actions import Node
@@ -10,14 +11,19 @@ import os
 
 def generate_launch_description():
 
+    gui = LaunchConfiguration('gui')
+
     baud_rate_launch_arg = DeclareLaunchArgument(
-      'baud_rate', default_value=TextSubstitution(text='9600')
+      'baud_rate', default_value=TextSubstitution(text='9600'), description="Baud rate for communication with qube"
     )
     device_launch_arg = DeclareLaunchArgument(
-      'device', default_value=TextSubstitution(text='COM1')
+      'device', default_value=TextSubstitution(text='COM1'), description="USB connection name"
     )
     simulation_launch_arg = DeclareLaunchArgument(
-      'simulation', default_value=TextSubstitution(text='false')
+      'simulation', default_value=TextSubstitution(text='false'), description="Enable simulation mode"
+    )
+    gui_launch_arg = DeclareLaunchArgument(
+        'gui', default_value=TextSubstitution(text='false'), description="Enable GUI for joint state publisher"
     )
 
     # Les xacro fil og viderekoble launch parametere, funksjon hentet og tilpasset fra https://robotics.stackexchange.com/questions/102165/ros2-python-launch-how-to-forward-launch-arguments-to-xacro-models
@@ -37,6 +43,7 @@ def generate_launch_description():
         baud_rate_launch_arg,
         device_launch_arg,
         simulation_launch_arg,
+        gui_launch_arg,
         create_robot_description_arg,
         # Kjør rviz2 med lagret konfigurasjonsfil.
         Node(
@@ -52,10 +59,17 @@ def generate_launch_description():
             output='screen',
             parameters=[{'robot_description': LaunchConfiguration('robot_desc')}]
         ),
-        # Kjør joint state publisher gui
+        # Kjør joint state publisher gui hvis 'gui' parameter er satt
         Node(
             package='joint_state_publisher_gui',
-            executable='joint_state_publisher_gui'
+            executable='joint_state_publisher_gui',
+            condition=IfCondition(gui)
+        ),
+        # Kjør joint state publisher uten gui hvis 'gui' parameter ikke er satt
+        Node(
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            condition=UnlessCondition(gui)
         ),
         # Kjør launch fil fra qube_driver pakke
         IncludeLaunchDescription(
